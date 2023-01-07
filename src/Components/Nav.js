e0a10d45e3bd72adab841a52e2ed62babf42a88f
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-07 15:32:37 +0545

    commit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,10 @@
-import earth from "/images/earth.png";
+import NavCSS from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <nav>
-      <img src={earth} alt="earth-logo" width="20px" />
+    <nav className={NavCSS["nav__content"]}>
+      <img src="/images/earth.png" alt="earth-logo" width="20px" />
+      <span>my travel journal.</span>
     </nav>
   );
 };
